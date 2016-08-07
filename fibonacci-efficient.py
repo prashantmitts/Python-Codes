97d815040e309655b8a9769cc8566601e6ab9197
Prashant Mittal <Prashant Mittal>
2016-08-08 00:42:57 +0530

    modified fibonacci program efficiency to log(n)

--- fibonacci-efficient.py
+++ fibonacci-efficient.py
@@ -1,21 +1,24 @@
-##efficient fibonacci calculation
-##using the matrix multiplication
+## efficient fibonacci calculation ##
+## using the matrix multiplication ##
 
-Q = [[0,1],[1,1]]
-I = [[1,0],[0,1]]
+## we can calculate fibonacci numbers upto #####    10^(10^5)  :)    #####
+## very quickly minded we use modulo  #####
+## else it will get crashed too much memory ######
+
+import sys
+sys.setrecursionlimit(10**6)
+
+Q = [[0,1],[1,1]]  ## fibonacci matrix 2X2
 
 def mul(q1,q2):
     k1 = ((q1[0])[0])*((q2[0])[0]) + ((q1[0])[1])*((q2[1])[0])
     k2 = ((q1[0])[0])*((q2[0])[1]) + ((q1[0])[1])*((q2[1])[1])
     k3 = ((q1[1])[0])*((q2[0])[0]) + ((q1[1])[1])*((q2[1])[0])
     k4 = ((q1[1])[0])*((q2[0])[1]) + ((q1[1])[1])*((q2[1])[1])
-    return ([[k1,k2],[k3,k4]])
-
-def add(q1,q2):
-    k1 = (q1[0])[0] + (q2[0])[0]
-    k2 = (q1[0])[1] + (q2[0])[1]
-    k3 = (q1[1])[0] + (q2[1])[0]
-    k4 = (q1[1])[1] + (q2[1])[1]
+    k1 = k1%(10**9 + 7)
+    k2 = k2%(10**9 + 7)
+    k3 = k3%(10**9 + 7)
+    k4 = k4%(10**9 + 7)
     return ([[k1,k2],[k3,k4]])
     
 def fib(n):
@@ -28,7 +31,6 @@ def fib(n):
         else:
             k = fib(n//2)
             return mul(mul(k,k),Q)
-
 
 def fibonacci(n):
     if (n == 0):
